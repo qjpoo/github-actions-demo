8e885dc6944604de0675536242789a0b96ee8229
qjpoo <qjpoo@163.com>
2019-11-19 06:37:26 +0000

    Deploying to gh-pages from master 6f1eb131a3c942eba8a603b5fe49a3a6387e2add

---
+++ precache-manifest.4cd9ca3380df10335423cd25d70a9edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcb3934cc59924f24814a06c0506f9cf",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "695d2f93258d4987f90a",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d2718bbd4f8fb500d63e",
+    "url": "/github-actions-demo/static/js/2.2f60c060.chunk.js"
+  },
+  {
+    "revision": "695d2f93258d4987f90a",
+    "url": "/github-actions-demo/static/js/main.b82d699c.chunk.js"
+  },
+  {
+    "revision": "dfb417f264dd525bbb87",
+    "url": "/github-actions-demo/static/js/runtime~main.553754d5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
